17f53314f4db25ddbb59ee5859b7e32060438271
zhang_ji <zhang_ji@worksap.co.jp>
2018-01-30 16:45:18 +0800

    [315] add binary search tree solution

--- 315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self.py
@@ -35,3 +35,46 @@ class Solution(object):
 solution = Solution()
 assert solution.countSmaller([5,2,6,1]) == [2,1,1,0]
 assert solution.countSmaller([-1,-1]) == [0, 0]
+
+
+class TreeNode(object):
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+        self.count = 1
+        self.left_tree_size = 0
+
+    def insert(self, val):
+        if self.val == val:
+            self.count += 1
+            return self.left_tree_size
+        elif val < self.val:
+            self.left_tree_size += 1
+            if self.left is None:
+                self.left = TreeNode(val)
+                return 0
+            return self.left.insert(val)
+
+        # right child
+        if self.right is None:
+            self.right = TreeNode(val)
+            return self.count + self.left_tree_size
+
+        return self.count + self.left_tree_size + self.right.insert(val)
+
+class Solution(object):
+    def countSmaller(self, nums):
+        if not nums:
+            return []
+
+        ret = [0]
+        root = TreeNode(nums[-1])
+        for n in reversed(nums[:-1]):
+            ret.append(root.insert(n))
+
+        return ret[::-1]
+
+solution = Solution()
+assert solution.countSmaller([5,2,6,1]) == [2,1,1,0]
+assert solution.countSmaller([-1,-1]) == [0, 0]
