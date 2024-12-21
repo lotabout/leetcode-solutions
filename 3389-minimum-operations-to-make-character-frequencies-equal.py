7096e815c1fdcbc43ea70b9d5a1cb2124b735ddc
Jinzhou Zhang <lotabout@gmail.com>
2024-12-21 21:42:50 +0800

    [3389] AC but not optimal

--- 3389-minimum-operations-to-make-character-frequencies-equal.py
+++ 3389-minimum-operations-to-make-character-frequencies-equal.py
@@ -8,23 +8,41 @@ def min_steps(cache: dict, counter: Counter, target: int, additional: int, ch: s
         return cache[cache_key][0]
 
     # could not move to next
+    next_ch = chr(ord(ch)+1)
     cur_step_count = counter[ch]+additional
     num_to_next_min = None
     steps_min = None
     if ch == 'z':
         steps_min = min(cur_step_count, abs(target - cur_step_count))
-    elif counter[chr(ord(ch)+1)] == 0:
-        next_step = min_steps(cache, counter, target, 0, chr(ord(ch)+1))
+    elif counter[next_ch] == 0:
+        next_step = min_steps(cache, counter, target, 0, next_ch)
         steps_min = min(cur_step_count, abs(target - cur_step_count)) + next_step
     else:
-        for num_to_next in range(0, cur_step_count+1):
-            kept = cur_step_count - num_to_next
-            next_step = min_steps(cache, counter, target, num_to_next, chr(ord(ch)+1))
-            current_step = min(kept, abs(target - kept))
-            steps_needed = next_step + current_step + num_to_next
-            if steps_min is None or steps_min > steps_needed:
-                steps_min = steps_needed
-                num_to_next_min = num_to_next
+        # will next ch needs help? next ch might choose two strategy
+        # 1. remove all characters, don't carry
+        # 2. align with target, carry if it is shorter than target
+
+        max_next_needed = max(target - counter[next_ch], 0)
+
+        mins_of_strategy = []
+
+        # 1 current ch tries to remove all
+        step_ch = cur_step_count
+        step_next_remove_all = min_steps(cache, counter, target, 0, next_ch)
+        step_next_align = min_steps(cache, counter, target, min(cur_step_count, max_next_needed), next_ch)
+        mins_of_strategy.append(step_ch + step_next_remove_all)
+        mins_of_strategy.append(step_ch + step_next_align)
+
+        # 2. current ch tries to align
+        max_available_for_carry = max(cur_step_count - target, 0)
+        step_ch = abs(cur_step_count - target)
+        step_next_remove_all = min_steps(cache, counter, target, 0, next_ch)
+        step_next_align = min_steps(cache, counter, target, min(max_available_for_carry, max_next_needed), next_ch)
+        mins_of_strategy.append(step_ch + step_next_remove_all)
+        mins_of_strategy.append(step_ch + step_next_align)
+
+        steps_min = min(mins_of_strategy)
+
     cache[cache_key] = (steps_min, num_to_next_min)
     return steps_min
 
@@ -32,7 +50,8 @@ def solution(s):
     cache = {}
     counter = Counter(s)
     max_height = max(counter.values())
-    result = min(min_steps(cache, counter, target, 0, 'a') for target in range(0, max_height+1))
+    steps = [min_steps(cache, counter, target, 0, 'a') for target in range(0, max_height+1)]
+    index, result = min(enumerate(steps), key=lambda x: x[1])
     return result
 
 class Solution(object):
